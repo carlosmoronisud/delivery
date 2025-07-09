03347f09f92e1b773db2b2f32f1b65c5ab5b24b4
Carlos Moroni Garcia <carlosmoronisud@gmail.com>
2025-07-09 19:39:12 -0300

    carroceis

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,58 +1,81 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { useContext, type ReactNode } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+
 function Footer() {
+
+    let data = new Date().getFullYear()
+
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    //Isso serve para mostrar um componente de maneira condicional
+    //Na tela de login, navbar e footer n aparece, e quando faz o login aparece
+    if(usuario.token !== ""){
+        component = (
   
-  return (
-    <footer className="bg-[#453E00] text-white py-14 px-6">
-      <div className="max-w-7xl mx-auto flex flex-wrap justify-between gap-12">
+ 
+      <footer className="bg-[#453E00] text-white py-14 px-6">
+        <div className="max-w-7xl mx-auto flex flex-wrap justify-between gap-12">
 
-        {/* Coluna 1 - Logo + Endereço */}
-        <div className="flex flex-col gap-6 min-w-[250px]">
-          <div className="flex items-center gap-4">
-            <img 
-              src="https://ik.imagekit.io/8h7kfljfc/imgs/deliverylogo.png?updatedAt=1752066792229"
-              alt="Logo"
-              className="w-16 h-16 rounded-full"
-            />
-            <p className="text-xl font-semibold">
-              Rua do Ouvidor 666<br />Campinas - SP
-            </p>
+          {/* Coluna 1 - Logo + Endereço */}
+          <div className="flex flex-col gap-6 min-w-[250px]">
+            <div className="flex items-center gap-4">
+              <img 
+                src="https://ik.imagekit.io/8h7kfljfc/imgs/deliverylogo.png?updatedAt=1752066792229"
+                alt="Logo"
+                className="w-16 h-16 rounded-full"
+              />
+              <p className="text-xl font-semibold">
+                Rua do Ouvidor 666<br />Campinas - SP
+              </p>
+            </div>
+            <p className="text-sm">+55 (19) 4992-8922</p>
+            <p className="text-sm">contact@delivery.com</p>
           </div>
-          <p className="text-sm">+55 (19) 4992-8922</p>
-          <p className="text-sm">contact@delivery.com</p>
+
+          {/* Coluna 2 - Navegação */}
+          <div className="flex flex-col gap-3 min-w-[150px]">
+            <h3 className="text-lg font-semibold mb-2">Mapa do Site</h3>
+            {["Home", "Sobre", "Produtos", "Categorias"].map((item, i) => (
+              <p
+                key={i}
+                className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer"
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+
+          {/* Coluna 3 - Social */}
+          <div className="flex flex-col gap-3 min-w-[150px]">
+            <h3 className="text-lg font-semibold mb-2">Social</h3>
+            {[ <LinkedinLogo size={48} weight='bold' />,
+             <InstagramLogo size={48} weight='bold' />,
+              <FacebookLogo size={48} weight='bold' />, "Linktree", "GitHub"].map((item, i) => (
+              <p
+                key={i}
+                className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer"
+              >
+                {item}
+              </p>
+            ))}
+          </div>
         </div>
 
-        {/* Coluna 2 - Navegação */}
-        <div className="flex flex-col gap-3 min-w-[150px]">
-          <h3 className="text-lg font-semibold mb-2">Mapa do Site</h3>
-          {["Home", "Sobre", "Produtos", "Categorias"].map((item, i) => (
-            <p
-              key={i}
-              className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer"
-            >
-              {item}
-            </p>
-          ))}
+        {/* Rodapé inferior */}
+        <div className="mt-10 text-center text-xs opacity-60">
+          © {new Date().getFullYear()} Delivery Company - Todos os direitos reservados.
         </div>
+      </footer>
+        )
+      }
 
-        {/* Coluna 3 - Social */}
-        <div className="flex flex-col gap-3 min-w-[150px]">
-          <h3 className="text-lg font-semibold mb-2">Social</h3>
-          {["Instagram", "Linkedin", "Linktree", "GitHub"].map((item, i) => (
-            <p
-              key={i}
-              className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer"
-            >
-              {item}
-            </p>
-          ))}
-        </div>
-      </div>
+  return (
+    {component}
+  )
 
-      {/* Rodapé inferior */}
-      <div className="mt-10 text-center text-xs opacity-60">
-        © {new Date().getFullYear()} Delivery Company - Todos os direitos reservados.
-      </div>
-    </footer>
-  );
 }
 
 export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,53 @@
-import { Link } from 'react-router-dom';
+import { useContext, type ReactNode } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
+import { ToastAlerta } from "../../utils/ToastAlerta"
 
 function Navbar() {
-  return (
-    <div className='flex px-15 py-6 justify-between bg-gray-50'>
-      <div className="pl-10">
-        <Link to="/">
-          <img
-            src="https://ik.imagekit.io/8h7kfljfc/imgs/deliverylogo.png?updatedAt=1752066792229"
-            alt="logo"
-            className="h-20 rounded-full"
-          />
-        </Link>
-      </div>
 
-      <div className="flex justify-between items-center gap-8 text-xl pr-10">
-        <Link to="/" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Home</Link>
-        <Link to="/clientes" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Categorias</Link>
-        <Link to="/oportunidades" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Produtos</Link>
-        <Link to="/sobre" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Sobre</Link>
-        
-      </div>
+    const navigate = useNavigate()
+
+    const { usuario, handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        ToastAlerta("O usuário foi desconectado com sucesso!", 'info')
+        navigate("/")
+    }
+
+    let component: ReactNode
+
+    if(usuario.token !== ""){
+        component = (
+ 
+        <div className='flex px-15 py-6 justify-between bg-gray-50'>
+          <div className="pl-10">
+            <Link to="/">
+              <img
+                src="https://ik.imagekit.io/8h7kfljfc/imgs/deliverylogo.png?updatedAt=1752066792229"
+                alt="logo"
+                className="h-20 rounded-full"
+              />
+            </Link>
+          </div>
+
+          <div className="flex justify-between items-center gap-8 text-xl pr-10">
+            <Link to="/" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Home</Link>
+            <Link to="/clientes" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Categorias</Link>
+            <Link to="/oportunidades" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Produtos</Link>
+            <Link to="/sobre" className="hover:scale-105 hover:text-yellow-300 transition-transform cursor-pointer">Sobre</Link>
+            
+          </div>
+        </div>
+        )
+      }
+
+  return (
+    <div>
+        {component}
     </div>
-  );
+  )
+  
 }
 
 export default Navbar;
